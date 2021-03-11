89a1f035104fe77a0b76a899e01f41d7e6b9fe8b
--add <andrewksmoker@gmail.com>
2021-03-11 12:54:20 -0500

    Set up App Name and Logo placement

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,14 +1,15 @@
 import logo from '../logo.svg';
 import '../App.css';
 import React, {Component} from 'react'
+import {Header, Image} from 'semantic-ui-react'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+        <Header as='h2'>
+          <Image circular src={logo} className="App-logo" alt="logo" />Brunchr
+        </Header> 
       </div>
     );
   }
