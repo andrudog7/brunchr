d0dde9f74b9ae180e31a080084a2ce221288a5cd
--add <andrewksmoker@gmail.com>
2021-03-22 17:09:58 -0400

    updating profile

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -24,7 +24,7 @@ class SignUpForm extends React.Component {
     }
 
     redirectToProfile = () => {
-        this.props.updateNavbar("profile")
+        this.props.updateNavbar("My Favorites")
             this.setState({
                 redirect: true
             })
