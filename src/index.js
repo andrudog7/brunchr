1215b04e28d05385effbf75819a3721f5df67087
--add <andrewksmoker@gmail.com>
2021-03-24 10:00:34 -0400

    update

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ import 'semantic-ui-css/semantic.min.css'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
 
-app.listen(port, function () {
-  console.log("Server started successfully")
-})
+// app.listen(port, function () {
+//   console.log("Server started successfully")
+// })
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
